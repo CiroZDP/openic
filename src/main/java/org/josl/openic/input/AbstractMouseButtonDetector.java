7f1306b16795a2320cc03e5ffe0cbf2a744c11bf
Ciro Diaz <cirod307@gmail.com>
2024-09-14 16:43:57 +0200

    Try to deploy and version 1.4.1

--- src/main/java/org/josl/openic/input/AbstractMouseButtonDetector.java
+++
@@ -1,4 +0,0 @@
-package org.josl.openic.input;
-
-public abstract class AbstractMouseButtonDetector extends Mouse implements MouseButtonDetector {
-}
--- src/main/java/org/josl/openic/input/MouseButtonDetector.java
+++
@@ -1,7 +0,0 @@
-package org.josl.openic.input;
-
-public interface MouseButtonDetector {
-
-    boolean isButtonPressed(int button);
-
-}
